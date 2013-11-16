c6cde6f9802b817c6a5b8d95ff6ae932578454ea
byme8 <silin.stanislav1993@yandex.ru>
2013-11-16 21:27:46 +0200

    Новая ветка!
    Один из способов выбора ячеек на форме:
    лкм выбор
    пкм отмена

--- BaldaGame/BaldaGame/Form1.Designer.cs
+++ BaldaGame/BaldaGame/Form1.Designer.cs
@@ -28,21 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(260, 238);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            this.dataGridView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- BaldaGame/BaldaGame/Form1.cs
+++ BaldaGame/BaldaGame/Form1.cs
@@ -16,10 +16,74 @@ namespace BaldaGame
             InitializeComponent();
         }
 
+        int[] cells;
+        DataGridViewCellStyle chek, notchek;
+
+        
         balda game;
         private void Form1_Load(object sender, EventArgs e)
         {
+
+            chek = new DataGridViewCellStyle();
+            notchek = new DataGridViewCellStyle();
+            chek.BackColor = Color.DodgerBlue;
+            notchek.BackColor = Color.White;
+            notchek.SelectionBackColor = Color.White;
+
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                    dataGridView1.Rows[i].Cells[j].Style = notchek;
+            cells = new int[0];
+
             game = new balda();
+            dataGridView1.RowCount = dataGridView1.ColumnCount = 6;
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+            {
+                dataGridView1.Columns[i].Width = 30;
+                dataGridView1.Columns[i].Resizable = DataGridViewTriState.False;
+                dataGridView1.Rows[i].Resizable = DataGridViewTriState.False;
+            }
+            var ch = game.CheckCells(new int[] { 0, 0, 0, 1, 1,1});
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int[] buf = new int[cells.Length+2];
+            for (int i = 0; i < cells.Length; i++)
+                buf[i] = cells[i];
+            dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style = chek;
+            buf[buf.Length - 2] = e.ColumnIndex;
+            buf[buf.Length - 1] = e.RowIndex;
+            cells = buf;
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!game.CheckCells(cells))
+            { 
+                ClearData();
+                MessageBox.Show("Некорректное выделение.");
+            }
+            if (e.Button == MouseButtons.Right)
+                ClearData();
+
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+                for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                    for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                        dataGridView1.Rows[i].Cells[j].Style = notchek;
+            cells = new int[0];
+        }
+
+        void ClearData()
+        {
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                    dataGridView1.Rows[i].Cells[j].Style = notchek;
+            cells = new int[0];
         }
     }
 }
